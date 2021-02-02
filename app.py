7155ea2e6a47d50d5279a259bdd0b1f1d048e08d
Colten Krauter <coltenkrauter@gmail.com>
2021-02-01 19:59:32 -0800

    Add comments and placeholders

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
+'''
+
+Binary Tree: In computer science, a binary tree is a tree data structure in which each 
+node has at most two children, which are referred to as the left child 
+and the right child.
+
+Binary Search Tree: In computer science, a binary search tree, also called an ordered or sorted 
+binary tree, is a rooted binary tree whose internal nodes each store a key 
+greater than all the keys in the node's left subtree and less than those in 
+its right subtree.
+
+Following are the basic operations of a tree,
+- Search: Searches an element in a tree.
+- Insert: Inserts an element in a tree.
+- Pre-order Traversal: Traverses a tree in a pre-order manner.
+- In-order Traversal: Traverses a tree in an in-order manner.
+- Post-order Traversal: Traverses a tree in a post-order manner.
+
+'''
+
 class Node:
-	def __init__(self, value):
-		self.left = None
-		self.right = None
-		self.value = value
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
 
 class BinaryTree:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
+
+    def search(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def pre_order(self):
+        pass
+
+    def in_order(self):
+        pass
+
+    def post_order(self):
+        pass
+
 
 tree = BinaryTree()
